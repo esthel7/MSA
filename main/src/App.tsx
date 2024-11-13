e24ab9651f394f06e27f329a3acd013cac657607
esthel <esthel9026@naver.com>
2024-11-14 03:09:41 +0900

    init: main

---
+++ main/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h3>This is main.</h3>
+    </div>
+  );
+}
+
+export default App;
