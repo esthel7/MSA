fce2331bf8ba00f11314444b5abb2a51f0ba6c86
esthel <esthel9026@naver.com>
2024-11-14 03:35:31 +0900

    init: sub

---
+++ sub/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import heart from './heart.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={heart} className="App-logo" alt="logo" />
+      <h3>This is sub.</h3>
+    </div>
+  );
+}
+
+export default App;
